b5e82ec5e3f5fa595b508f2a53604fc469b43f0a
Benjamin Soyka <bensoyka@icloud.com>
2019-12-01 13:49:28 -0800

    Improve 2019 day 1

--- 2019/day1/part1.py
+++ 2019/day1/part1.py
@@ -1,4 +1,3 @@
-import math
 with open("input.txt") as f:
-	lines = [math.floor(int(line.strip())/3)-2 for line in f.readlines()]
-print(sum(lines))
+    lines = [int(line.strip()) // 3 - 2 for line in f.readlines()]
+print(sum(lines))  # 3271095
