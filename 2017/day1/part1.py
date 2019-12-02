d2c5601ac9cc1b3977a3e935fbe8b4fbd88ca199
Benjamin Soyka <bensoyka@icloud.com>
2019-12-01 19:59:29 -0800

    Add 2017 days 1 and 4

---
+++ 2017/day1/part1.py
@@ -0,0 +1,9 @@
+with open("input.txt") as f:
+    start = f.readlines()[0].strip()
+total = 0
+for index_1, char_1 in enumerate(start):
+    index_2 = (index_1 + 1) % len(start)
+    char_2 = start[index_2]
+    if char_1 == char_2:
+        total += int(char_1)
+print(total)  # 1223
---
+++ 2017/day4/part1.py
@@ -0,0 +1,7 @@
+with open("input.txt") as f:
+    start = [line.strip().split(" ") for line in f.readlines()]
+result = 0
+for password in start:
+    if len(password) == len(set(password)):
+        result += 1
+print(result)
