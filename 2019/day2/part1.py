a649350b409205b5197d2ebe1d22fb3cceb2b453
Benjamin Soyka <bensoyka@icloud.com>
2019-12-03 07:08:08 -0800

    Add 2019 day 2

---
+++ 2019/day2/part1.py
@@ -0,0 +1,13 @@
+with open("input.txt") as f:
+    txt = list(map(int, f.readlines()[0].strip().split(",")))
+pos = 0
+while True:
+    print(pos)
+    if txt[pos] == 1:
+        txt[txt[pos+3]] = txt[txt[pos+1]] + txt[txt[pos+2]]
+    elif txt[pos] == 1:
+        txt[txt[pos+3]] = txt[txt[pos+1]] * txt[txt[pos+2]]
+    elif txt[pos] == 99:
+        break
+    pos += 4
+print(txt)
---
+++ 2019/day2/part2.py
@@ -0,0 +1,32 @@
+def reset():
+    with open("input.txt") as f:
+        return list(map(int, f.readlines()[0].strip().split(",")))
+
+
+txt = reset()
+pos = 0
+
+
+def result(noun, verb):
+    global txt
+    global pos
+    txt[1], txt[2] = noun, verb
+    while True:
+        print(len(txt), pos)
+        if txt[pos] == 1:
+            txt[txt[pos + 3]] = txt[txt[pos + 1]] + txt[txt[pos + 2]]
+        elif txt[pos] == 1:
+            txt[txt[pos + 3]] = txt[txt[pos + 1]] * txt[txt[pos + 2]]
+        elif txt[pos] == 99:
+            break
+        pos += 4
+    pos = 0
+    return txt[0]
+
+
+for noun in range(100):
+    for verb in range(100):
+        txt = reset()
+        print(result(noun, verb))
+        if result(noun, verb) == 19690720:
+            print((100 * noun) + verb)
