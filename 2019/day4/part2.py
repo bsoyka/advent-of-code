c3923aa40f96a4c66a364bdaef377cb1e93f63c5
Benjamin Soyka <bensoyka@icloud.com>
2019-12-04 15:31:57 -0800

    Replace `str(check)`s

--- 2019/day4/part2.py
+++ 2019/day4/part2.py
@@ -5,16 +5,17 @@ with open("input.txt") as f:
     start, stop = map(int, f.readlines()[0].strip().split("-"))
 valid = 0
 for check in range(start, stop + 1):
+    check = str(check)
     repeats = 0
-    if len(str(check)) != 6:
+    if len(check) != 6:
         continue
-    if not re.match(r"^1*2*3*4*5*6*7*8*9*$", str(check)):
+    if not re.match(r"^1*2*3*4*5*6*7*8*9*$", check):
         continue
     for x in digits:
-        check = str(check).replace(x*5, "")
-        check = str(check).replace(x*4, "")
-        check = str(check).replace(x*3, "")
-        repeats += str(check).count(x * 2)
+        check = check.replace(x*5, "")
+        check = check.replace(x*4, "")
+        check = check.replace(x*3, "")
+        repeats += check.count(x * 2)
     if not repeats > 0:
         continue
     valid += 1
