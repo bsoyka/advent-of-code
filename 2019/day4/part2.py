6f5cf88e6a7b2ac5c2939e768098b7e982fc8b7f
Benjamin Soyka <bensoyka@icloud.com>
2019-12-04 19:50:17 -0800

    Format 2019d4p2 for PEP8 compliance

--- 2019/day4/part2.py
+++ 2019/day4/part2.py
@@ -12,9 +12,9 @@ for check in range(start, stop + 1):
     if not re.match(r"^1*2*3*4*5*6*7*8*9*$", check):
         continue
     for x in digits:
-        check = check.replace(x*5, "")
-        check = check.replace(x*4, "")
-        check = check.replace(x*3, "")
+        check = check.replace(x * 5, "")
+        check = check.replace(x * 4, "")
+        check = check.replace(x * 3, "")
         repeats += check.count(x * 2)
     if not repeats > 0:
         continue
