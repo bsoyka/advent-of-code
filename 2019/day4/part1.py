ff6f0bde104955b791a559e3176bb0efab294db5
Benjamin Soyka <bensoyka@icloud.com>
2019-12-04 19:47:28 -0800

    Consolidate `str`s

--- 2019/day4/part1.py
+++ 2019/day4/part1.py
@@ -4,14 +4,15 @@ with open("input.txt") as f:
     start, stop = map(int, f.readlines()[0].strip().split("-"))
 valid = 0
 for check in range(start, stop + 1):
+    check = str(check)
     repeats = 0
-    if len(str(check)) != 6:
+    if len(check) != 6:
         continue
     for x in digits:
-        repeats += str(check).count(x * 2)
+        repeats += check.count(x * 2)
     if not repeats > 0:
         continue
-    if not all(x <= y for x, y in zip(str(check), str(check)[1:])):
+    if not all(x <= y for x, y in zip(check, check[1:])):
         continue
     valid += 1
 print(valid)  # 511
