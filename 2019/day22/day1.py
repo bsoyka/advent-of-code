338b1337b5d52ca2ceac3e170d527330b49a4ad3
Benjamin Soyka <bensoyka@icloud.com>
2019-12-23 11:44:20 -0800

    Add 2019d22p1

---
+++ 2019/day22/day1.py
@@ -0,0 +1,26 @@
+import re
+
+regex = r"([a-z ]+) (-?\d+)"
+with open("input.txt") as f:
+    instructions = [line.strip() for line in f.readlines()]
+
+deck = list(range(10007))
+for instruction in instructions:
+    search = re.search(regex, instruction)
+    if search is None:
+        if instruction == "deal into new stack":
+            deck = list(reversed(deck))
+    else:
+        todo, num = search.groups()[0], int(search.groups()[1])
+        if todo == "cut":
+            deck = deck[num:] + deck[:num]
+        elif todo == "deal with increment":
+            new = [None for _ in range(len(deck))]
+            current = 0
+            for card in deck:
+                new[current] = card
+                current += num
+                while current > len(deck)-1:
+                    current -= len(deck)
+            deck = list(new)
+print(deck.index(2019))
