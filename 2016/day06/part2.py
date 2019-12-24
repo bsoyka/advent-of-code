52104d38c1a8a64a562e0a368110e255d03faf6d
Benjamin Soyka <bensoyka@icloud.com>
2019-12-24 09:40:29 -0800

    Add 2016d6p2

---
+++ 2016/day06/part2.py
@@ -0,0 +1,10 @@
+with open("input.txt") as f:
+    lines = [line.strip() for line in f]
+columns = [[] for _ in range(len(lines[0]))]
+final = []
+for line in lines:
+    for col_index, char in enumerate(line):
+        columns[col_index].append(char)
+for col in columns:
+    final.append(min(set(col), key=col.count))
+print("".join(final))
