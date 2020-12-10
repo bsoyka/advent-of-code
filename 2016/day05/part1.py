d4fda1036468f211c1bf7826ca1087cc92f3511b
Benjamin Soyka <bensoyka@icloud.com>
2020-12-10 08:59:38 -0700

    Refactor 2016 day 5 part 1

--- 2016/day05/part1.py
+++ 2016/day05/part1.py
@@ -1,18 +1,23 @@
-import re
-import hashlib
+from hashlib import md5
+from pathlib import Path
+from re import compile
 
-with open("input.txt") as f:
+with (Path(__file__).parent / "input.txt").open() as f:
     prefix = f.read().strip()
-regex = re.compile(r"^0{5}.")
+
+regex = compile(r"0{5}.")
+
 index = 0
 final = ""
+
 for _ in range(8):
     while True:
-        m = hashlib.md5()
-        m.update(f"{prefix}{index}".encode("utf-8"))
+        m = md5(f"{prefix}{index}".encode("utf-8")).hexdigest()
+
         index += 1
-        if regex.match(m.hexdigest()):
-            print(index, m.hexdigest(), m.hexdigest()[5])
-            final += m.hexdigest()[5]
+
+        if regex.match(m):
+            final += m[5]
             break
-print(final)
+
+print(final)  # 1a3099aa
