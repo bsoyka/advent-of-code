afda7e98ddc248bd459c8d0f10968072e9dfa569
Ben Soyka <bensoyka@icloud.com>
2021-11-30 23:45:06 -0700

    Add logging to 2021/1/1

--- 2021/day01/part1.py
+++ 2021/day01/part1.py
@@ -1,12 +1,17 @@
 # Input data loading
 from aocd import get_data
 
+# Simple logging
+from loguru import logger
+
 depths = list(map(int, get_data(year=2021, day=1).splitlines()))
+logger.debug("Loaded depths list")
 
 result = sum(
     1 if depths[index - 1] < depth else 0
     for index, depth in enumerate(depths)
     if index != 0
 )
+logger.debug("Calculated result")
 
-print(result)
+logger.success(result)
