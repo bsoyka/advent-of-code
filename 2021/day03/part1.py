c1aa665082c66444a5d38433817ce052161260bd
Ben Soyka <bensoyka@icloud.com>
2021-12-03 06:27:41 -0700

    Add 2021d3

---
+++ 2021/day03/part1.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import Counter
+
+# Simple logging
+from loguru import logger
+
+# Personal utilities
+from bsoyka_aoc_utils import get_data
+
+DIAGNOSTICS: List[str] = get_data(2021, 3, split_lines=True)
+logger.debug("Loaded diagnostics list")
+
+NUMBER_LENGTH = len(DIAGNOSTICS[0])
+
+column_bit_counts = [Counter() for _ in range(NUMBER_LENGTH)]
+
+# Get the number of 0s and 1s in each column
+for row in DIAGNOSTICS:
+    for index, col in enumerate(row):
+        # Add the bit to the counter, keeping it as a string
+        column_bit_counts[index][col] += 1
+
+gamma: str = ""
+epsilon: str = ""
+
+for col_counts in column_bit_counts:
+    # Add the most common bit to the gamma string
+    gamma += col_counts.most_common()[0][0]
+
+    # Add the least common bit to the epsilon string
+    epsilon += col_counts.most_common()[-1][0]
+
+# Convert from binary strings to integers
+GAMMA_RESULT = int(gamma, 2)
+EPSILON_RESULT = int(epsilon, 2)
+
+logger.info("Final gamma rate: {} ({})", GAMMA_RESULT, gamma)
+logger.info("Final epsilon rate: {} ({})", EPSILON_RESULT, epsilon)
+
+logger.success("Result: {}", GAMMA_RESULT * EPSILON_RESULT)
---
+++ 2021/day03/part2.py
@@ -0,0 +1,63 @@
+from typing import List
+
+# Simple logging
+from loguru import logger
+
+# Personal utilities
+from bsoyka_aoc_utils import get_data
+
+DIAGNOSTICS: List[str] = get_data(2021, 3, split_lines=True)
+logger.debug("Loaded diagnostics list")
+
+NUMBER_LENGTH = len(DIAGNOSTICS[0])
+
+# Make a copy of the diagnostics for each value
+oxygen_possible = DIAGNOSTICS.copy()
+co2_possible = DIAGNOSTICS.copy()
+
+# Loop through the bits
+for bit_index in range(NUMBER_LENGTH):
+    if len(oxygen_possible) > 1:
+        # Narrow down the oxygen possibilities, count the 0s and 1s
+        oxy_one_count, oxy_zero_count = 0, 0
+
+        for entry in oxygen_possible:
+            if entry[bit_index] == "1":
+                oxy_one_count += 1
+            else:
+                oxy_zero_count += 1
+
+        MOST_COMMON = "1" if oxy_one_count >= oxy_zero_count else "0"
+
+        # Remove the ones that don't match this most common bit
+        oxygen_possible = [
+            entry
+            for entry in oxygen_possible
+            if entry[bit_index] == MOST_COMMON
+        ]
+
+    if len(co2_possible) > 1:
+        # Narrow down the CO2 possibilities, count the 0s and 1s
+        co2_one_count, co2_zero_count = 0, 0
+
+        for entry in co2_possible:
+            if entry[bit_index] == "1":
+                co2_one_count += 1
+            else:
+                co2_zero_count += 1
+
+        MOST_COMMON = "0" if co2_one_count >= co2_zero_count else "1"
+
+        # Remove the ones that don't match this most common bit
+        co2_possible = [
+            entry for entry in co2_possible if entry[bit_index] == MOST_COMMON
+        ]
+
+# Convert from binary strings to integers
+OXYGEN_RESULT = int(oxygen_possible[0], 2)
+CO2_RESULT = int(co2_possible[0], 2)
+
+logger.info("Final oxygen reading: {} ({})", OXYGEN_RESULT, oxygen_possible[0])
+logger.info("Final CO2 reading: {} ({})", CO2_RESULT, co2_possible[0])
+
+logger.success("Result: {}", OXYGEN_RESULT * CO2_RESULT)
