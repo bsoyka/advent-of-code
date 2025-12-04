d4c56217638a56b4b13596f690cf1611e0ff7c3c
Ben Soyka <hello@bsoyka.me>
2025-12-03 23:48:46 -0600

    feat(2025): add d4p2

---
+++ 2025/day04/part2.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+
+from loguru import logger
+
+with (Path(__file__).parent / "input.txt").open() as f:
+    INPUT_ROWS = f.readlines()
+
+logger.debug("Loaded input data")
+
+
+def adjacent_positions(
+    row_position: int,
+    col_position: int,
+    total_rows: int = len(INPUT_ROWS),
+    total_cols: int = len(INPUT_ROWS[0]) - 1,  # Subtract 1 for the newline
+) -> set[tuple[int, int]]:
+    """Gather the adjacent grid positions to a cell.
+
+    At most, this is the 8 cells surrounding the cell. However, this function
+    accounts for cells on the edges or corners of the grid, returning fewer adjacent
+    cells if applicable.
+
+    Args:
+        row_position: The row index of the cell.
+        col_position: The column index of the cell.
+        total_rows: The number of rows in the grid.
+        total_cols: The number of columns in the grid.
+
+    Returns: A set of the adjacent (row, col) cells to the given cell.
+    """
+    min_row = max(0, row_position - 1)
+    max_row = min(total_rows - 1, row_position + 1)
+
+    min_col = max(0, col_position - 1)
+    max_col = min(total_cols - 1, col_position + 1)
+
+    positions: set[tuple[int, int]] = {
+        (adj_row, adj_col)
+        for adj_row in range(min_row, max_row + 1)
+        for adj_col in range(min_col, max_col + 1)
+    }
+
+    positions.discard((row_position, col_position))
+
+    return positions
+
+
+def forklifts_in_positions(
+    positions: set[tuple[int, int]], input_data: list[str]
+) -> int:
+    """Calculates the number of forklifts (@s) present in the given positions.
+
+    Args:
+        positions: A set of (row, col) cells to check for forklifts.
+        input_data: The list of rows given as input.
+
+    Returns: The number of forklifts present in the given cells.
+    """
+    forklifts = 0
+
+    for cell_row, cell_col in positions:
+        if input_data[cell_row][cell_col] == "@":
+            forklifts += 1
+
+    return forklifts
+
+
+def get_removable_forklifts(input_data: list[str]) -> set[tuple[int, int]]:
+    """Get the currently removable forklifts in a grid.
+
+    Args:
+        input_data: The list of rows given as input.
+
+    Returns: A set of (row, col) cells containing forklifts that can be removed.
+    """
+    removable: set[tuple[int, int]] = set()
+
+    for row_index, row in enumerate(input_data):
+        row = row.strip()
+
+        for col_index, cell in enumerate(row):
+            if cell != "@":
+                continue
+
+            adjacent_cells = adjacent_positions(row_index, col_index)
+            adjacent_forklifts = forklifts_in_positions(adjacent_cells, input_data)
+
+            if adjacent_forklifts < 4:
+                removable.add((row_index, col_index))
+
+    return removable
+
+
+removed = 0
+current_data = [row for row in INPUT_ROWS]
+
+while removable_forklifts := get_removable_forklifts(current_data):
+    for forklift_row, forklift_col in removable_forklifts:
+        row = current_data[forklift_row].rstrip("\n")
+        row = row[:forklift_col] + "." + row[forklift_col + 1 :]
+        current_data[forklift_row] = row + "\n"
+        removed += 1
+
+logger.success("Result: {}", removed)
