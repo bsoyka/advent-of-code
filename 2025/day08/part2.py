ba0a2acbf854fa9177349c6d4ff3d0b7a2039319
Ben Soyka <hello@bsoyka.me>
2025-12-08 13:01:41 -0600

    feat(2025): add d8p2

---
+++ 2025/day08/part2.py
@@ -0,0 +1,59 @@
+from itertools import combinations
+from pathlib import Path
+from sys import exit
+
+from loguru import logger
+from part1 import calculate_distance, find_junction_in_circuit_list
+
+
+def pair_junctions_by_distance(
+    junctions: list[str], limit: int | None = 1000
+) -> list[tuple[float, str, str]]:
+    """Makes pairs of junctions in ascending order by the distance between them.
+
+    Args:
+        junctions: The list of junctions to consider.
+        limit: The maximum number of pairs to return.
+
+    Returns: A list of tuples of (distance, junction1, junction2).
+    """
+    junction_pairs = combinations(junctions, 2)
+    distance_results = [
+        (calculate_distance(j1, j2), j1, j2) for j1, j2 in junction_pairs
+    ]
+
+    return (
+        sorted(distance_results)[:limit]
+        if limit is not None
+        else sorted(distance_results)
+    )
+
+
+if __name__ == "__main__":
+    INPUT_LINES = (Path(__file__).parent / "input.txt").read_text().splitlines()
+    circuits = [
+        {
+            j,
+        }
+        for j in INPUT_LINES
+    ]
+    junctions_to_link = pair_junctions_by_distance(INPUT_LINES, None)
+
+    for _, junction1, junction2 in junctions_to_link:
+        j1_circuit_index = find_junction_in_circuit_list(junction1, circuits)
+        j2_circuit_index = find_junction_in_circuit_list(junction2, circuits)
+
+        j1_circuit, j2_circuit = circuits[j1_circuit_index], circuits[j2_circuit_index]
+
+        joined_circuit = j1_circuit.union(j2_circuit)
+
+        for i in sorted({j1_circuit_index, j2_circuit_index}, reverse=True):
+            # Pop starting with the latest to avoid the earlier element shifting
+            circuits.pop(i)
+
+        circuits.append(joined_circuit)
+
+        if len(circuits) == 1:
+            j1x, j2x = int(junction1.split(",")[0]), int(junction2.split(",")[0])
+            logger.success("Result: {}", j1x * j2x)
+            exit()
